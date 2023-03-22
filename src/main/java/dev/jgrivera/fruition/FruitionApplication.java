93b4c86876f33469228f3e769912d13e4f7e7631
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-03-22 20:07:48 +0800

    feat: add initializr files

---
+++ src/main/java/dev/jgrivera/fruition/FruitionApplication.java
@@ -0,0 +1,13 @@
+package dev.jgrivera.fruition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FruitionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jgrivera/fruition/FruitionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jgrivera.fruition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
