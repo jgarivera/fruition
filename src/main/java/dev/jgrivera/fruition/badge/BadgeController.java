5e0eb746cafb8446eefe88a47b9dfb29bdafef5c
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-03-29 21:24:47 +0800

    feat(badge): add request mapping to badge controller

--- src/main/java/dev/jgrivera/fruition/badge/BadgeController.java
+++ src/main/java/dev/jgrivera/fruition/badge/BadgeController.java
@@ -4,12 +4,14 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/badges")
 public class BadgeController {
 
     private final BadgeRepository repository;
