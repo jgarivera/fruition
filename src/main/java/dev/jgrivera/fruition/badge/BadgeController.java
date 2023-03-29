a98b2a4b37a4989a68bb17022bff56b9941ebe8c
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-03-29 13:50:00 +0800

    feat(badge): return response entities in controller methods

--- src/main/java/dev/jgrivera/fruition/badge/BadgeController.java
+++ src/main/java/dev/jgrivera/fruition/badge/BadgeController.java
@@ -1,5 +1,6 @@
 package dev.jgrivera.fruition.badge;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +18,20 @@ public class BadgeController {
     }
 
     @GetMapping("/")
-    public Iterable<Badge> getBadges() {
-        return repository.findAll();
+    public ResponseEntity<Iterable<Badge>> getBadges() {
+        Iterable<Badge> badges = repository.findAll();
+
+        return ResponseEntity.ok().body(badges);
     }
 
     @GetMapping("/{id}")
-    public Optional<Badge> getBadge(@PathVariable String id) {
-        return repository.findById(UUID.fromString(id));
+    public ResponseEntity<Badge> getBadge(@PathVariable String id) {
+        Optional<Badge> badge = repository.findById(UUID.fromString(id));
+
+        if (badge.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok().body(badge.get());
+        }
     }
 }
