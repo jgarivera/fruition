aaa7aee3a7fa7f52ae936ad96d5822b9577af867
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-04-11 21:33:26 +0800

    feat(badge): add find by name method to badge repository

--- src/main/java/dev/jgrivera/fruition/badge/BadgeRepository.java
+++ src/main/java/dev/jgrivera/fruition/badge/BadgeRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.UUID;
 
 public interface BadgeRepository extends CrudRepository<Badge, UUID> {
+    Badge findByName(String name);
 }
