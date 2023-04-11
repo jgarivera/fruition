3b6939241509a65e48124a8ffc74d3be3c9b2066
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-04-11 21:32:40 +0800

    feat(badge): add badges to context of home page

--- src/main/java/dev/jgrivera/fruition/badge/BadgePageController.java
+++ src/main/java/dev/jgrivera/fruition/badge/BadgePageController.java
@@ -2,15 +2,30 @@ package dev.jgrivera.fruition.badge;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/badges")
 public class BadgePageController {
 
+    private final BadgeRepository repository;
+
+    public BadgePageController(BadgeRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping(path = {"", "/"}, produces = MediaType.TEXT_HTML_VALUE)
-    public String viewHomePage() {
+    public String viewHomePage(Model model) {
+        List<Badge> badges = new ArrayList<>();
+        repository.findAll().forEach(badges::add);
+
+        model.addAttribute("badges", badges);
+
         return "index";
     }
 }
