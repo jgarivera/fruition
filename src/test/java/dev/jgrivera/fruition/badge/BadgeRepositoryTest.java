dab32086adecc78155dfc41c6a2764b03611f97a
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-04-18 22:13:00 +0800

    test(badge): add test cases in badge repository test

--- src/test/java/dev/jgrivera/fruition/badge/BadgeRepositoryTest.java
+++ src/test/java/dev/jgrivera/fruition/badge/BadgeRepositoryTest.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import java.util.NoSuchElementException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -17,7 +19,33 @@ public class BadgeRepositoryTest {
     private BadgeRepository repository;
 
     @Test
-    public void it_gets_badges_by_name() throws Exception {
+    public void it_gets_badges() {
+        Badge badge1 = new Badge("test 1");
+        entityManager.persist(badge1);
+
+        Badge badge2 = new Badge("test 2");
+        entityManager.persist(badge2);
+
+        entityManager.flush();
+
+        Iterable<Badge> badges = repository.findAll();
+
+        assertThat(badges).contains(badge1, badge2);
+    }
+
+    @Test
+    public void it_gets_badge_by_id() throws NoSuchElementException {
+        Badge badge = new Badge("test");
+        entityManager.persist(badge);
+        entityManager.flush();
+
+        Badge foundBadge = repository.findById(badge.getId()).orElseThrow();
+
+        assertThat(foundBadge.getId()).isEqualTo(badge.getId());
+    }
+
+    @Test
+    public void it_gets_badge_by_name() {
         Badge badge = new Badge("test");
         entityManager.persist(badge);
         entityManager.flush();
